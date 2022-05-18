223212f928df01120353dea46b0e0b3e41646e45
tahoupt <tom@houptlab.org>
2022-05-18 09:58:08 -0400

    Initial Commit

---
+++ livecameras/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  livecameras
+//
+//  Created by Tom Houpt on 22/5/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
